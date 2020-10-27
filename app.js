757eb28a4c5e7b2a199981b4dd8f175ac6ac4a4c
Chido Ukaigwe <chidodesigns@gmail.com>
2020-10-27 11:05:38 +0000

    Monster Slayer Game Complete

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
-            winner: null
+            winner: null,
+            logMessages: []
         };
     },
 
@@ -21,24 +22,28 @@ const app = Vue.createApp({
             this.monsterHealth = 100;
             this.winner = null;
             this.currentRound = 0;
+            this.logMessages = []
         },
 
         attackMonster() {
             this.currentRound++;
             let attackValue = getRandomValue(5, 12);
             this.monsterHealth = this.monsterHealth - attackValue;
+            this.addLogMessage('player', 'attack', attackValue);
             this.attackPlayer();
         },
         //  Max Value 15 - Lowest Value 8
         attackPlayer() {
             let attackValue = getRandomValue(8, 15);
             this.playerHealth = this.playerHealth - attackValue;
+            this.addLogMessage('monster', 'attack', attackValue);
         },
 
         specialAttackMonster() {
             this.currentRound++;
             let attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'special-attack', attackValue);
             this.attackPlayer();
         },
 
@@ -50,11 +55,21 @@ const app = Vue.createApp({
             } else {
                 this.playerHealth += healValue;
             }
+            this.addLogMessage('player', 'heal', healValue);
             this.attackPlayer();
         },
 
         surrender() {
             this.winner = 'monster';
+        },
+
+        addLogMessage(who, what, value) {
+            //  Add Log At The Beginning Of The Array
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            });
         }
 
     },
