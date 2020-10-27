ba8a9daae775c1e6a8aa8e7bc6ae62b97ae3bb4b
Chido Ukaigwe <chidodesigns@gmail.com>
2020-10-27 09:44:03 +0000

    added a heal functionality

--- app.js
+++ app.js
@@ -32,6 +32,17 @@ const app = Vue.createApp({
             let attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
+        },
+
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100
+            } else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
         }
 
     },
