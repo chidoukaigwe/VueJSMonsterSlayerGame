223f2d9c5cbf71f476332ce2d6db74f46a55faac
Chido Ukaigwe <chidodesigns@gmail.com>
2020-10-27 10:01:27 +0000

    Added a game over screen

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
+            winner: null
         };
     },
 
@@ -62,6 +63,32 @@ const app = Vue.createApp({
             return this.currentRound % 3 !== 0;
         }
 
+    },
+
+    watch: {
+
+        playerHealth(value) {
+
+            if (value <= 0 && this.monsterHealth <= 0) {
+
+                this.winner = 'draw';
+
+            } else if (value <= 0) {
+                //  Player lost
+                this.winner = 'monster'
+            }
+
+        },
+
+        monsterHealth(value) {
+
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'player';
+            }
+
+        }
     }
 
 });
